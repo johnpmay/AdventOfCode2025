e587c6052916bf10d8bfd0d542ea96c0e73659e0
John May <johnmay@gmail.com>
2025-12-12 18:54:55 -0800

    More Day 12 attempts

--- Day12/Day12-pulp.py
+++ Day12/Day12-pulp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[45]:
+# In[ ]:
 
 
 from pathlib import Path
@@ -14,7 +14,7 @@ input_file = Path('../../AdventOfCode_inputs/AoC-2025-12-input.txt')
 rinput = input_file.read_text()
 
 
-# In[46]:
+# In[ ]:
 
 
 sinput = '''0:
@@ -53,13 +53,13 @@ sinput = '''0:
 '''
 
 
-# In[47]:
+# In[ ]:
 
 
-ainput = sinput
+ainput = rinput
 
 
-# In[48]:
+# In[ ]:
 
 
 [*shapes, trees] = ainput.split('\n\n')
@@ -67,13 +67,13 @@ shapes = [ s.splitlines()[1:]  for s in shapes]
 trees = [ [list(map(int,a[0].split("x"))), list(map(int, a[1].split())) ] for a in [t.split(': ') for t in trees.splitlines()]]
 
 
-# In[49]:
+# In[ ]:
 
 
 svols = [sum( shapes[j][i].count('#') for i in range(3)) for j in range(6)]
 
 
-# In[263]:
+# In[ ]:
 
 
 def shape2Num(shape):
@@ -103,13 +103,13 @@ S = sorted(list(set.union(*Slist)))
 masks = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,0), (0,1), (1,-1), (1,0), (1,1)]
 
 
-# In[264]:
+# In[ ]:
 
 
-shape2Num(["#..","...", "..."])
+S
 
 
-# In[265]:
+# In[ ]:
 
 
 num2Shape( 256 ^ 502 )
@@ -117,10 +117,10 @@ num2Shape( 256 ^ 502 )
 
 # **Part One**
 
-# In[274]:
+# In[ ]:
 
 
-from z3 import *
+from pulp import *
 
 startTime = time()
 count = 0
@@ -128,61 +128,69 @@ count = 0
 for t in range(len(trees)):
 
     h, w = trees[t][0]
-    solve = Solver()
-
+    solve = LpProblem("TreeProblem-%s"%t)
+    
     # variables: T[x,y,s] = 1 <==> there is a tile s at pos x,y
-    T = {(i,j,s):Int("T__%s,%s,%s" % (i,j,s)) for s in S for j in range(w) for i in range(h) }
-
+    #T = {(i,j,s):Int("T__%s,%s,%s" % (i,j,s)) for s in S for j in range(w) for i in range(h) }
+    T = LpVariable.dicts("T", (range(-1,h+1),range(-1,w+1),S), cat="Binary")
+    
     # Tiles cannot be positioned on the edges
     for i in range(-1,h+1):
         for s in S:
-            T[i,-1,s] = 0
-            T[i,0,s] = 0
-            T[i,w-1,s] = 0
-            T[i,w,s] = 0
+            T[i][-1][s] = 0
+            T[i][0][s] = 0
+            T[i][w-1][s] = 0
+            T[i][w][s] = 0
     for j in range(-1,w+1):
         for s in S:
-            T[-1,j,s] = 0
-            T[0,j,s] = 0
-            T[h-1,j,s] = 0
-            T[h,j,s] = 0
-
+            T[-1][j][s] = 0
+            T[0][j][s] = 0
+            T[h-1][j][s] = 0
+            T[h][j][s] = 0
+            
     # the T's are 0 or 1
     for i in range(1,h-1):
         for j in range(1,w-1):
             for s in S:
-                solve.add(T[i,j,s]>=0)
-                solve.add(T[i,j,s]<=1)
+                solve+=(T[i][j][s]>=0)
+                solve+=(T[i][j][s]<=1)
 
     # only one s at each x,y
     for i in range(h):
         for j in range(w):
-            solve.add(sum(T[i,j,s] for s in S)<=1)
+            solve+=(sum(T[i][j][s] for s in S)<=1)
 
     # exactly the correct number of each s
     for k in range(len(shapes)):
-        solve.add(sum(sum(T[i,j,s] for i in range(h) for j in range(w)) for s in Slist[k]) == trees[t][1][k])
+        solve+=(sum(sum(T[i][j][s] for i in range(h) for j in range(w)) for s in Slist[k]) == trees[t][1][k])
 
     # no overlaps
     for i in range(h):
         for j in range(w):
-            solve.add( sum( sum(T[i+masks[k][0],j+masks[k][1],s]*((s//2**k)%2) for s in S) for k in range(9)) <= 1 )
+            solve+=( sum( sum(T[i+masks[k][0]][j+masks[k][1]][s]*((s//2**k)%2) for s in S) for k in range(9)) <= 1 )
+    
+    solve.solve()
+    if solve.status > 0:
+        count+=1
+                                               
+print("%s s"%(time()-startTime))  
+ans1 = count
 
-    if solve.check() :
-        print(t, trees[t], 'SAT')
-        m = solve.model()
-        for i in range(1,h-1):
-            for j in range(1,w-1):
-                for s in S:
-                    if m[T[i,j,s]].as_long():
-                        print((i,j),s)
-                        for l in num2Shape(s):
-                            print(l)
-                        print('\n')
-        count += 1
-    else:
-        print(t, trees[t], 'UNSAT')
 
-print("%s s"%(time()-startTime))
-count
+# In[ ]:
+print("\n\n\n\n\n\n\n")
+
+print("ans1 = %s"%ans1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
